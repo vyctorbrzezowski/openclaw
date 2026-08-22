b9cb5147e27c8da29b9c688bc6f5fab4539c2f89
vyctorbrzezowski <krzyszchweski@gmail.com>
2026-08-22 06:45:48 -0300

    test(ui): default mock locale to English

--- scripts/control-ui-mock-dev.ts
+++ scripts/control-ui-mock-dev.ts
@@ -2895,7 +2895,7 @@ function createMockGatewayPlugin(scenario: ControlUiMockGatewayScenario): Plugin
     transformIndexHtml(html) {
       return html.replace(
         "</head>",
-        `    <script data-openclaw-control-ui-mock-gateway>\n${initScript}\n${statefulInitScript}\n    </script>\n  </head>`,
+        `    <script data-openclaw-control-ui-mock-gateway>\nlocalStorage.setItem("openclaw.i18n.locale", "en");\n${initScript}\n${statefulInitScript}\n    </script>\n  </head>`,
       );
     },
   };
